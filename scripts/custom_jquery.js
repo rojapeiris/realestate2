badf92bda5d8daeb895eedf85188efcd0e91bedc
Rojith Peiris <rojithonline@gmail.com>
2024-09-17 18:43:35 +0530

    Update custom_jquery.js

--- scripts/custom_jquery.js
+++ scripts/custom_jquery.js
@@ -261,28 +261,7 @@
             });
         }
 
-        function searchTypeButtons() {
-            $('.search-type label.active input[type="radio"]').prop('checked', true);
-            var buttonWidth = $('.search-type label.active').width();
-            var arrowDist = $('.search-type label.active').position().left;
-            $('.utf-search-type-arrow').css('left', arrowDist + (buttonWidth / 2));
-            $('.search-type label').on('change', function() {
-                $('.search-type input[type="radio"]').parent('label').removeClass('active');
-                $('.search-type input[type="radio"]:checked').parent('label').addClass('active');
-                var buttonWidth = $('.search-type label.active').width();
-                var arrowDist = $('.search-type label.active').position().left;
-                $('.utf-search-type-arrow').css({
-                    'left': arrowDist + (buttonWidth / 2),
-                    'transition': 'left 0.4s cubic-bezier(.87,-.41,.19,1.44)'
-                });
-            });
-        }
-        if ($(".utf-main-search-form-item").length) {
-            searchTypeButtons();
-            $(window).on('load resize', function() {
-                searchTypeButtons();
-            });
-        }
+        
         var config = {
             '.chosen-select': {
                 disable_search_threshold: 10,
